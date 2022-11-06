57c5db505927c53c082c4c3855cc7417f3d9d35b
SergioVeloso10 <sergiofelveloso@gmail.com>
2022-11-05 22:07:03 -0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/cliente', function () {
+    return view('cliente');
+});
+
+//Route::get('/cliente',[ClienteController::class,'store'])->name('cliente');
+
+//Route::post('agregarCliente',[ClienteController::class, 'store'])->name('agregarCliente');
